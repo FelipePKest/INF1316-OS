39a3275e9a7264caeb988b851315ecc2f652d8aa
Felipe Kestelman <fpkest@gmail.com>
2020-09-17 13:02:42 -0300

    Incio lab 3 finalizado

---
+++ labs/lab3/main.c
@@ -0,0 +1,45 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUM_THREADS 2
+
+void * cont_crescente(void * thread_id) {
+    printf("%d: Hello World!\n", thread_id);
+    /* do other things */
+    for (int i = 0; i<20; i++) {
+        printf("%d \n",i);
+        sleep(1);
+    }
+    pthread_exit(NULL); /*not necessary*/ 
+}
+
+void * cont_decrescente(void * thread_id) {
+    printf("%d: Hello World!\n", thread_id);
+    /* do other things */
+    for (int i = 30; i>0; i--) {
+        printf("%d \n",i);
+        sleep(2);
+    }
+    pthread_exit(NULL); /*not necessary*/ 
+}
+ 
+int main(void) {
+    pthread_t threads[NUM_THREADS];
+    int t;
+    
+    for(t=0;t < NUM_THREADS;t++) {
+        printf("Creating thread %d\n", t);
+        switch (t) {
+        case (0):
+            pthread_create(&threads[t], NULL, cont_crescente,(void *)t);
+        case (1):
+            pthread_create(&threads[t], NULL, cont_decrescente,(void *)t);
+        }
+    }
+    
+    for(t=0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t],NULL); /* wait for all the threads to terminate*/
+    }
+    
+    return 0;
+} 
