97d2f4252729c7e0f8f44111866d5765df184ae3
Felipe Kestelman <fpkest@gmail.com>
2020-09-26 00:28:46 -0300

    Created Struct to pass as argument

--- labs/lab3/main_lab.c
+++ labs/lab3/main_lab.c
@@ -1,40 +1,51 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
-#include  <sys/ipc.h>
+#include <sys/ipc.h>
 #include <unistd.h>
 
 #define NUM_THREADS 8
 
-void soma_no_vetor(int * vet, int indice_inicial) {
-  int i = indice_inicial;
-  for (i;i<(indice_inicial+1024/NUM_THREADS);i++) {
-    vet[1024]+=vet[i];
+typedef struct args {
+  int * vec;
+  int index;
+} Args;
+
+void soma_no_vetor(Args * args) {
+  int i = args->index;
+  for (;i<(args->index+1024/NUM_THREADS);i++) {
+    args->vec[1024]+=args->vec[i];
   }
 }
 int main(void) {
-  int segmento, *p, id, pid, status;
+  int segmento, *p;
   // aloca a memória compartilhada com o tamanho do vetor de 900 inteiros
   segmento = shmget(IPC_PRIVATE, sizeof (int)*1025, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
-  // associa a memória compartilhada ao processo
+  // // associa a memória compartilhada ao processo
   p = (int *) shmat (segmento, 0, 0); // comparar o retorno com -1
   pthread_t threads[NUM_THREADS];
-  
-  int t;
 
-  srand(time(0));
+  // // srand(time(0));
       
-  // preencher o vetor com numeros aleatorios
+  // // preencher o vetor com numeros aleatorios
   for (int i = 0;i<1024;i++) {
     p[i] = rand()%256;
   }
-
-  for (int t;t<NUM_THREADS;t++) {
-    pthread_create(&threads[t],NULL,soma_no_vetor,p,t*(1024/NUM_THREADS));
+  Args * a = (Args *)malloc(sizeof(Args));
+  a->vec = p;
+  a->index = 0;
+  for (int j=0;j<NUM_THREADS;j++) {
+    a->index = j*(1024/NUM_THREADS);
+    pthread_create(&threads[j],NULL,soma_no_vetor,a);
   }
-
+	for(int k = 0; k < NUM_THREADS; k++) {
+		pthread_join(threads[k], NULL);
+  }
+  free(a);
+  shmdt(p);
+	shmctl(IPC_RMID,0,0);
   return 0;
-
-} 
+}
